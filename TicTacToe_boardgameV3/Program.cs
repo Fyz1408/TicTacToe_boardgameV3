384f9216f2d8cb028feef4e2a1d4f682192dd8aa
Daniel Lykke Hansen <dlh@evercall.dk>
2022-04-06 10:54:12 +0200

    Added temporary test code in program

--- TicTacToe_boardgameV3/Program.cs
+++ TicTacToe_boardgameV3/Program.cs
@@ -6,7 +6,33 @@ namespace TicTacToe_boardgameV3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string name;
+            string playerPick;
+
+            Console.WriteLine("Enter player 1 name");
+            
+            name = Console.ReadLine();
+            
+            Console.WriteLine("Welcome {0} !", name);
+
+            Console.WriteLine("Pick nought or cross:");
+            Console.WriteLine("O for nought \nX for cross");
+
+            playerPick = Console.ReadLine();
+            
+            if (playerPick == "O" || playerPick == "o" || playerPick == "X" || playerPick == "x")
+            {
+                Console.WriteLine("Valid!");
+            } else
+            {
+                Console.WriteLine("Invalid! ");
+            }
+             
+            Console.WriteLine("You chose {0}", playerPick);
+
+            Player p1 = new Player(name, playerPick);
+
         }
     }
 }
