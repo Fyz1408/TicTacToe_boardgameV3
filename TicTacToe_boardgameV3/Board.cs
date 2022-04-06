4ab5259551589fa83cf3ae79261602304905371f
Almi0098 <85223371+Almi0098@users.noreply.github.com>
2022-04-06 11:00:57 +0200

    Field og Board

---
+++ TicTacToe_boardgameV3/Board.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_boardgame
+{
+    //Constructor
+    public class Board
+    {
+        public int BoardID;
+        public abstract CurrentTurn();
+        //public List<Field>
+        public char[] arr = { '1', '2', '3', '3', '4', '5', '6', '7', '8', '9' };
+
+        //Boad metode der laver boardet
+        public void CreateBoard()
+        {
+            Console.WriteLine("     |    |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", arr[1], arr[2], arr[3]);
+            Console.WriteLine("     |    |     ");
+            Console.WriteLine("     |    |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", arr[4], arr[5], arr[6]);
+            Console.WriteLine("     |    |     ");
+            Console.WriteLine("     |    |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", arr[7], arr[8], arr[9]);
+            Console.WriteLine("     |    |     ");
+
+        }
+    }
+}
---
+++ TicTacToe_boardgameV3/Fields.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_boardgameV3
+{
+    public class Field
+    {
+        public int FieldID { get; set; }
+        public bool IsFieldEmpty;
+    }
+}
