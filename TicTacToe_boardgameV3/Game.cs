a365d29d20770a73f91f28bce9abd776b3c1bc7c
Daniel Lykke <daniellykke1@gmail.com>
2022-04-07 12:34:42 +0200

    Added StartGame and TakeTurn methods

--- TicTacToe_boardgameV3/Game.cs
+++ TicTacToe_boardgameV3/Game.cs
@@ -16,90 +16,130 @@ namespace TicTacToe_boardgameV3
         public Game()
         {
             var bd = new Board();
-            var rnd = new Random();
-            
+
             int playerCount = 2;
+            int currentTurn = 0;
             bool showIntro = true;
+            int playerPick;
 
             while (GameStatus != 1 && GameStatus != -1)
             {
-                if (showIntro)
-                {
-                    Players = new List<Player>();
-                    for (int i = 1; i <= playerCount; i++)
-                    {
-                        Players.Add(
-                            new Player(i)
-                        );
-                    }
-            
-                    Console.Clear();
-
-                    Console.WriteLine("Welcome to tic tac toe");
-                
-                    foreach (var p in Players)
-                    {
-                        Console.WriteLine("Enter player {0} name:", p.PlayerID);
-                        p.PlayerName = Console.ReadLine();
-                    }
-            
-                    Console.Clear();
-
-                    Console.WriteLine("Welcome {0} and {1}! \n", Players[0].PlayerName, Players[1].PlayerName);
-            
-                    Console.WriteLine("{0} will play with noughts: O and {1} will play with crosses: X \n", Players[0].PlayerName, Players[1].PlayerName);
-            
-                    Console.WriteLine("Lets get started, press a key when you are ready to move on\n");
-
-                    Console.ReadKey();
-                    Console.Clear();
-            
-                    Console.Write("Randomizing who will start... \n");
-            
-                    int playerIdToStart = rnd.Next(0, 3);
-
-                    var pgb = new ProgressBar();
-                    using (pgb) {
-                        for (int i = 0; i <= 100; i++) {
-                            pgb.Report((double) i / 100);
-                            Thread.Sleep(20);
-                        }
-                    }
-
-                    Console.Clear();
-            
-                    foreach (var p in Players)
-                    {
-                        if (p.PlayerID == playerIdToStart)
-                        {
-                            Console.WriteLine("{0} was chosen to start! Click when you're ready to play!", p.PlayerName);
-                        }
-                    }
-
-                    Console.ReadKey();
-                }
-                
                 Console.Clear();
                 
-                Console.WriteLine("Its your turn {0}");
+                if (showIntro)
+                {
+                    currentTurn = StartGame(playerCount);
+                    showIntro = false;
+                }
+
+                Console.WriteLine("{0}:{1} and {2}:{3}", Players[0].PlayerName,  Players[0].PlayerType, Players[1].PlayerName,  Players[1].PlayerType);
+
+                foreach (var p in Players)
+                {
+                    if (currentTurn == p.PlayerID)
+                    {
+                        Console.WriteLine("{0}'s turn\n", p.PlayerName);
+                    }
+                }
+
+                bd.CreateBoard();
                 
-                bd.CreateBoard();    
+                while (!int.TryParse(Console.ReadLine(), out playerPick))
+                {
+                    Console.WriteLine("Not valid ");
+                }
+                
+                TakeTurn(currentTurn, playerPick, bd);
                 
                 Console.ReadKey();
+                
+                ValidateResults(bd);
                 
             }
         }
 
-        public void StartGame(Player[] Players, Board board)
+        private int StartGame( int playerCount)
         {
+            var rnd = new Random();
+            Players = new List<Player>();
+                    
+            for (int i = 1; i <= playerCount; i++)
+            { 
+                Players.Add(
+                    new Player(i)
+                    );
+            }
             
+            Console.Clear();
+
+            Console.WriteLine("Welcome to tic tac toe");
+                
+            foreach (var p in Players)
+            { 
+                Console.WriteLine("Enter player {0} name:", p.PlayerID);
+                p.PlayerName = Console.ReadLine();
+            }
             
+            Console.Clear();
+
+            Console.WriteLine("Welcome {0} and {1}! \n", Players[0].PlayerName, Players[1].PlayerName);
             
+            Console.WriteLine("{0} will play with noughts: O and {1} will play with crosses: X \n", Players[0].PlayerName, Players[1].PlayerName);
+           
+            Console.WriteLine("Lets get started, press a key when you are ready to move on\n");
+
+            Console.ReadKey();
+            Console.Clear();
+            
+            Console.Write("Randomizing who will start... \n");
+
+            int playerIdToStart = rnd.Next(0, playerCount+1);
+                    
+            var pgb = new ProgressBar();
+            using (pgb) {
+                for (int i = 0; i <= 100; i++) {
+                    pgb.Report((double) i / 100);
+                    Thread.Sleep(20);
+                }
+            }
+
+            Console.Clear();
+            
+            foreach (var p in Players)
+            {
+                if (p.PlayerID == playerIdToStart)
+                {
+                    Console.WriteLine("{0} was chosen to start! Click when you're ready to play!", p.PlayerName);
+                }
+            }
+            
+            return playerIdToStart; 
         }
 
-        public void takeTurn( Player PlayersTurn, int FieldID)
+        private void TakeTurn( int currentTurn, int FieldPicked, Board bd)
         {
-            
+            if (bd.arr[FieldPicked] != 'X' && bd.arr[FieldPicked] != 'O')
+            {
+                //if the turn is player 2 then mark O else mark X  
+                if (currentTurn % 2 == 0) 
+                {
+                    bd.arr[FieldPicked] = 'O';
+                    currentTurn++;
+                }
+                else
+                {
+                    bd.arr[FieldPicked] = 'X';
+                    currentTurn++;
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Sorry the row {0} is already marked with {1}\n", FieldPicked, bd.arr[FieldPicked]);
+                Console.WriteLine("Please wait 2 seconds while the board is loading again.....");
+                Thread.Sleep(2000);
+
+            }
         }
 
         private void ValidateResults(Board board)
