581a10c5d06932aaa07d680cde05d1a35e90908a
Daniel Lykke <daniellykke1@gmail.com>
2022-04-07 10:31:34 +0200

    Added progressBar and intro text

--- TicTacToe_boardgameV3/Game.cs
+++ TicTacToe_boardgameV3/Game.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
 using TicTacToe_boardgame;
 
 namespace TicTacToe_boardgameV3
 {
     public class Game
     {
-        public Player[] Players;
+        public List<Player> Players;
         
         public Board Board;
 
@@ -13,46 +15,79 @@ namespace TicTacToe_boardgameV3
 
         public Game()
         {
-            Board bd = new Board();
-            Player p1 = new Player();
-            Player p2 = new Player();
+            var bd = new Board();
+            var rnd = new Random();
             
-            Console.WriteLine("Welcome to tic tac toe");
-                
-            Console.WriteLine("Enter player 1 name:");
-                
-            p1.PlayerName = Console.ReadLine();
-                
-            Console.WriteLine("Welcome {0} !", p1.PlayerName);
-            
-            Console.WriteLine("Enter player 2's name:");
-            
-            p2.PlayerName = Console.ReadLine();
-            
-            Console.WriteLine("Welcome {0} !", p2.PlayerName);
-            
-            Console.WriteLine("{0} Will play with noughts: O and {1} will play with crosses: X", p1.PlayerName, p2.PlayerName);
-            
-            Console.WriteLine("Lets get started, Click when you're ready to play!");
-
-            Console.ReadKey();
-            
-            Console.Clear();
-            
-            bd.CreateBoard();
+            int playerCount = 2;
+            bool showIntro = true;
 
             while (GameStatus != 1 && GameStatus != -1)
             {
+                if (showIntro)
+                {
+                    Players = new List<Player>();
+                    for (int i = 1; i <= playerCount; i++)
+                    {
+                        Players.Add(
+                            new Player(i)
+                        );
+                    }
+            
+                    Console.Clear();
+
+                    Console.WriteLine("Welcome to tic tac toe");
                 
+                    foreach (var p in Players)
+                    {
+                        Console.WriteLine("Enter player {0} name:", p.PlayerID);
+                        p.PlayerName = Console.ReadLine();
+                    }
+            
+                    Console.Clear();
+
+                    Console.WriteLine("Welcome {0} and {1}! \n", Players[0].PlayerName, Players[1].PlayerName);
+            
+                    Console.WriteLine("{0} will play with noughts: O and {1} will play with crosses: X \n", Players[0].PlayerName, Players[1].PlayerName);
+            
+                    Console.WriteLine("Lets get started, press a key when you are ready to move on\n");
+
+                    Console.ReadKey();
+                    Console.Clear();
+            
+                    Console.Write("Randomizing who will start... \n");
+            
+                    int playerIdToStart = rnd.Next(0, 3);
+
+                    var pgb = new ProgressBar();
+                    using (pgb) {
+                        for (int i = 0; i <= 100; i++) {
+                            pgb.Report((double) i / 100);
+                            Thread.Sleep(20);
+                        }
+                    }
+
+                    Console.Clear();
+            
+                    foreach (var p in Players)
+                    {
+                        if (p.PlayerID == playerIdToStart)
+                        {
+                            Console.WriteLine("{0} was chosen to start! Click when you're ready to play!", p.PlayerName);
+                        }
+                    }
+
+                    Console.ReadKey();
+                }
+                
+                Console.Clear();
+                
+                Console.WriteLine("Its your turn {0}");
+                
+                bd.CreateBoard();    
+                
+                Console.ReadKey();
                 
             }
-            Console.WriteLine(p2.PlayerType);
-            
-            Console.ReadKey();
-            Console.WriteLine(p2.PlayerType);
-            
-            Console.ReadKey();
-
         }
 
         public void StartGame(Player[] Players, Board board)
--- TicTacToe_boardgameV3/Player.cs
+++ TicTacToe_boardgameV3/Player.cs
@@ -3,13 +3,16 @@ namespace TicTacToe_boardgameV3
 {
     public class Player
     {
+        public int PlayerID { get; set; }
+        
         public string PlayerName { get; set; }
+        
         public Token.TokenType PlayerType;
         
-        public void getPlayerName()
+        public Player(int playerId)
         {
-            
-            
+            PlayerID = playerId;
+
         } 
         
 
