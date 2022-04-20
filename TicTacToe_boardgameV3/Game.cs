e54bc55fa89c29a0051c8d39111e45a922a9c53d
Daniel Lykke <daniellykke1@gmail.com>
2022-04-20 12:40:21 +0200

    Added comments to describe whats happening in the code

--- TicTacToe_boardgameV3/Game.cs
+++ TicTacToe_boardgameV3/Game.cs
@@ -8,27 +8,33 @@ namespace TicTacToe_boardgameV3
 {
     public class Game
     {
+        // Gamestatus, 0 = ongoing, 1 = a player has won, -1 its a draw
         private int GameStatus;
 
         public Game()
-        { 
+        {
             bool intro = true;
             
             while (intro)
             {
                 
+                // Creating board and players
                 Board bd = new Board();
                 Player p1 = new Player();
                 Player p2 = new Player();
 
+                // Set gamestatus to ongoing
                 GameStatus = 0;
                 
+                // StartGame method returns the player that should begin
                 int currentTurn = StartGame( p1, p2);
 
                 Console.Clear();
                 
                 while (GameStatus != 1 && GameStatus != -1)
                 {
+                    // currentTurn modulus 2 if theres a remainder after dividing its p1's turn else its p2
+                    // currentTurn gets increased by 1 at the end of every turn 
                     int currentPlayerId = currentTurn % 2 != 0 ? 1 : 0;
                     Player cpi = currentPlayerId == 1 ? p1 : p2;
                     
@@ -36,6 +42,8 @@ namespace TicTacToe_boardgameV3
                     Console.WriteLine("{0}:{1} and {2}:{3}", p1.Name, p1.Token, p2.Name, p2.Token);
                     Console.WriteLine("Its {0}'s turn, pick a spot to place your token: {1}", cpi.Name, cpi.Token);
 
+                    // If its the first time the user is playing the game we need to create the board
+                    // if not we'll just display it
                     if (intro)
                     {
                         bd.CreateBoard();
@@ -46,11 +54,12 @@ namespace TicTacToe_boardgameV3
                         bd.DisplayBoard();
                     }
 
-                    int playerPick;
+                    // The only valid input a user is allowed to enter
                     int[] validInput = {1, 2, 3, 4, 5, 6, 7, 8, 9};
                     bool validPick = true;
 
-                    if (int.TryParse(Console.ReadLine(), out playerPick))
+                    // Verify the input is a number and is from 1-9
+                    if (int.TryParse(Console.ReadLine(), out int playerPick))
                     {
                         if (!validInput.Contains(playerPick))
                         {
@@ -62,9 +71,10 @@ namespace TicTacToe_boardgameV3
                         validPick = false;
                     }
                
-
+                    // If the input was valid we will run the TakeTurn method
                     if (validPick)
                     {
+                        // TakeTurn returns true or false based if it placed a token or the spot was occupied 
                         if ( TakeTurn(cpi.Token, playerPick, bd)) 
                         {
                             currentTurn++;
@@ -79,6 +89,8 @@ namespace TicTacToe_boardgameV3
                         Console.WriteLine("Invalid input, please try again ");
                         displayProgressbar(20);
                     }
+                    
+                    // Validate all rows to see if anyone has won or a draw has happend 
                     ValidateResults(bd);
                     
                     bd.DisplayBoard();
@@ -95,7 +107,8 @@ namespace TicTacToe_boardgameV3
                 }
 
                 Console.WriteLine("To play again enter P, to quit enter Q");
-
+                
+                // If the user wants to play again we set intro to true and start from scratch
                 string input = Console.ReadLine();
                 if (input != null) intro = input.ToUpper() == "P";
             }
@@ -103,12 +116,12 @@ namespace TicTacToe_boardgameV3
 
         private int StartGame( Player p1, Player p2)
         {
-            var rnd = new Random();
 
             Console.Clear();
 
             Console.WriteLine("Welcome to tic tac toe");
             
+            // Getting the players name 
             for (int i = 1; i <= 2; i++)
             {
                 Console.WriteLine("Enter player {0} name:", i);
@@ -118,8 +131,8 @@ namespace TicTacToe_boardgameV3
                 
             }
 
+            // Assigning token types to each player 
             p1.Token = Token.TokenType.X;
-            
             p2.Token = Token.TokenType.O;
 
             Console.Clear();
@@ -135,6 +148,9 @@ namespace TicTacToe_boardgameV3
             
             Console.Write("Randomizing who will start... \n");
 
+            var rnd = new Random();
+            
+            // Randomly chosen if p1 or p2 will start
             int playerToStart = rnd.Next(0, 2+1);
             var pts = playerToStart == 1 ? p1 : p2;
                     
@@ -145,13 +161,16 @@ namespace TicTacToe_boardgameV3
             Console.WriteLine("{0} was chosen to start! Click when you're ready to play!", pts.Name);
             Console.ReadKey();
                 
+            // Return the randomly chosen player
             return playerToStart; 
         }
 
         private bool TakeTurn( Token.TokenType tokenType, int fieldPicked, Board bd)
         {
+            // Check the field isn't occupied first
             if (bd.Fields[fieldPicked].FieldState != "X" && bd.Fields[fieldPicked].FieldState != "O")
             {
+                // If not we set the token in the field
                 bd.Fields[fieldPicked].FieldState = tokenType.ToString();
                 return true;
             }
